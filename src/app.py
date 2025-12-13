55169ff614a5c9f4084acf5ac797c2f62d97b321
Varwig-BA <hdvarwig@gmail.com>
2025-12-12 19:01:20 -0600

    Adding fxns to app.py

--- src/app.py
+++ src/app.py
@@ -1,2 +1,32 @@
+import math
+
 def add(a,b):
     return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return float(a) / float(b)
+
+def logarithm(a,b):
+    return math.log(a,b)
+
+def square(a):
+    return a**2
+
+def sine(a):
+    return math.sin(a)
+
+def cosine(a):
+    return math.cos(a)
+
+def sqrt(a):
+    math.sqrt(a)
+
+# Find the percentage of a/b
+def percentage(a,b):
+    float(a) / float(b) * 100
