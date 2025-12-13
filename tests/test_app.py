65183642379ab756452a97211f063d4ddf5e2eaa
Varwig-BA <hdvarwig@gmail.com>
2025-12-12 19:26:22 -0600

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
-import ../src/app as app
+import sys
+from pathlib import Path
+
+root = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(root / "src"))
+
+import app
 
 def test_add():
   assert app.add(5,6) == 11
